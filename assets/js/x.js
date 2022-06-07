f5ec8d97563b381d8191bffad17be0521e6b1bd7
Sayoko123f <63059711+Sayoko123f@users.noreply.github.com>
2022-06-07 13:23:43 +0800

    add dark mode

--- assets/js/x.js
+++ assets/js/x.js
@@ -1,6 +1,20 @@
 'use strict';
 window.addEventListener('load', () => {
-  document.querySelector('#goto-top-button').addEventListener('click', () => {
-    window.scroll({ top: 0, left: 0, behavior: 'smooth' });
-  });
+    // go top button
+    document.querySelector('#goto-top-button').addEventListener('click', () => {
+        window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+    });
+    //  light, dark toggle
+    document
+        .querySelector('#light-mode-button')
+        .addEventListener('click', () => {
+            localStorage.setItem('theme', 'dark');
+            document.documentElement.classList.add('dark');
+        });
+    document
+        .querySelector('#dark-mode-button')
+        .addEventListener('click', () => {
+            localStorage.setItem('theme', 'light');
+            document.documentElement.classList.remove('dark');
+        });
 });
